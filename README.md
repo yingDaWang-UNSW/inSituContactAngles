0348bafef3dab6a3db53231c310f4f8512ad139d
yingDaWang-UNSW <yingda.wang@unsw.edu.au>
2024-02-26 09:46:35 +1100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # inSituContactAngles
 In Situ Contact Angle Measurements on Voxelized Images
 
-1. open matlab and navigate to the inSituContactAngles folder
-2. Run "runThisFile.m" in Matlab
+This codebase is companion to the publication "In situ Characterization of Heterogeneous Surface Wetting in Porous Materials" by YD Wang et al./, published in Advances in Colloids and Interface Sciences.
+
+It computes the local contact angle - mapped to the contact loop of fluid clusters in segmented voxelised images containing 2 fluid phases and 1 solid phase. 
+
+Users can compute the contact angle distributions of their whole domain, and/or interpolate the spatial contact angle map over their solid domain.
+
+This codebase uses 3 methods of measuring the insitu contact angle:
+
+1. Geometric method (Alratrout, 2018) https://doi.org/10.1016/j.advwatres.2017.07.018
+2. Topological method (Sun, 2020) https://doi.org/10.1016/j.jcis.2020.05.076
+3. Extended Topological method (this study)
+
+To run this code and its example, see the following steps:
+
+
+0. Users should first open the provided 3D tiff image file "segmentedRockTestTheta30.tif" in imageJ or another visualiser to better understand the data that this codebase works with as input 
+
+1. Open matlab and navigate to the inSituContactAngles folder
+
+2. Run "runThisFile.m" in Matlab. This will plot the contact angle distributions as the computation runs, but will also at the end dump a database "thetaData0.mat". This contains all the information required to reconstruct the results and perform further analysis.
+
+3. Run "generateSpatialMap.m" in Matlab. This will take "thetaData0.mat" and perform spatial analysis. It will create both 3D tiff image files containing the contact angle in the voxels corresponding to the contact loops of the fluid-solid phases as well as the 3D interpolation of the values in these contact loops over the entire solid phase.
+
--- generateSpatialMap.m
+++ generateSpatialMap.m
@@ -1,5 +1,7 @@
 addpath(genpath(pwd))
 
+resamplingFactor=10;
+
 %% load the contact angle data
 thetaData0=load('thetaData0.mat');
 thetaData0=thetaData0.thetaData0;
@@ -17,7 +19,6 @@ domainDirect=solid;
 domainSunLines=solid;
 domainKearneyLines=solid;
 domainDirectLines=solid;
-% ds=bwdist(solid);
 thetaVec=[];
 for i=numel(thetaData0)-1:-1:1
     cluster=thetaData0{i};
@@ -27,7 +28,6 @@ for i=numel(thetaData0)-1:-1:1
         domainSun(cluster.domainInds)=uint8(mean(cluster.thetaMacroSun));
         domainKearney(cluster.domainInds)=uint8(quantile(cluster.thetaMacroKearney,0.5));
         
-        % identify the contact lines voxels and map value
         x=max([min(x)-3,1]):min([max(x)+3,size(solid,1)]);
         y=max([min(y)-3,1]):min([max(y)+3,size(solid,2)]);
         z=max([min(z)-3,1]):min([max(z)+3,size(solid,3)]);
@@ -38,7 +38,6 @@ for i=numel(thetaData0)-1:-1:1
         dClusters=bwdist(domainSunClusters);
         dOtherPhase=bwdist(bbDomain==0);
         dInternalSolid=bwdist(~(bbDomain==1));
-        % the contact line voxels within the solid are 
         solidContactLineVoxels=dInternalSolid<1.8 & dInternalSolid>0 & dClusters<2.8 & dClusters>0 & dOtherPhase<1.8 & dOtherPhase>0;
         if ~isempty(cluster.directTheta)
             domainDirect(cluster.domainInds)=uint8(mean(cluster.directTheta));
@@ -48,39 +47,16 @@ for i=numel(thetaData0)-1:-1:1
             
             bbSolid(solidContactLineVoxels)=uint8(mean(cluster.thetaMacroSun));
             domainSunLines(x,y,z)=bbSolid;
-%             
+
             if abs(quantile(cluster.thetaMacroKearney,0.5)-mean(cluster.directTheta))>abs((180-quantile(cluster.thetaMacroKearney,0.5))-mean(cluster.directTheta))
                 cluster.thetaMacroKearney=180-cluster.thetaMacroKearney;
             end
             
             bbSolid(solidContactLineVoxels)=uint8(quantile(cluster.thetaMacroKearney,0.5));
             domainKearneyLines(x,y,z)=bbSolid;
-%             thetaVec=[thetaVec;mean(thetaActual),mean(cluster.directTheta),quantile(cluster.thetaMacroKearney,0.5),mean(cluster.thetaMacroSun)];
-
-%         else
-%             bbSolid(solidContactLineVoxels)=uint8(mean(cluster.thetaMacroSun));
-%             domainSunLines(x,y,z)=bbSolid;
-%             bbSolid(solidContactLineVoxels)=uint8(quantile(cluster.thetaMacroKearney,0.5));
-%             domainKearneyLines(x,y,z)=bbSolid;
-%             thetaVec=[thetaVec;mean(thetaActual),mean(cluster.directTheta),quantile(cluster.thetaMacroKearney,0.5),mean(cluster.thetaMacroSun)];
             disp([num2str(i),' of ', num2str(numel(thetaData0)),', Direct: ', num2str(mean(cluster.directTheta)), ', Kearney: ', num2str(quantile(cluster.thetaMacroKearney,0.5)), ' Sun: ', num2str(mean(cluster.thetaMacroSun))])
-%             sum(solidContactLineVoxels(:))
         end
-%         if ~isempty(cluster.directTheta)
-%             disp([num2str(i),' of ', num2str(numel(thetaData0)),' Actual: ', num2str(mean(thetaActual)), ', Direct: ', num2str(mean(cluster.directTheta)), ', Kearney: ', num2str(quantile(cluster.thetaMacroKearney,0.5)), ' Sun: ', num2str(mean(cluster.thetaMacroSun))])
-%             thetaVec=[thetaVec;mean(thetaActual),mean(cluster.directTheta),quantile(cluster.thetaMacroKearney,0.5),mean(cluster.thetaMacroSun)];
-%         end
-%         radiusVsTheta=[radiusVsTheta;numel(cluster.domainInds).^(1/3),mean(cluster.thetaMacroSun),mean(cluster.thetaMacroKearney),mean(cluster.directTheta)];
-%         figure(1);
-%         plot(thetaVec(:,1),thetaVec(:,2:4),'o')
-%         xlim([0 180])
-%         ylim([0 180])
-% %         hold on
-%         drawnow
-%         step=100;
-%         scatter3(x(1:step:end),y(1:step:end),z(1:step:end))
-%         drawnow
-%         hold on
+
     end
 end
 writeTiffStackYDW('./domainSun.tif',domainSun);
@@ -91,7 +67,6 @@ writeTiffStackYDW('./domainDirect.tif',domainDirect);
 writeTiffStackYDW('./domainDirectLines.tif',domainDirectLines);
 %% use the loop data and interpolate over the solid
 
-resamplingFactor=10;
 
 domainSunLinesAll=readTiffStackYDW('./domainSunLines.tif');
 domainKearneyLinesAll=readTiffStackYDW('./domainKearneyLines.tif');
@@ -100,17 +75,13 @@ domainDirectLinesAll=readTiffStackYDW('./domainDirectLines.tif');
 data=imresize3(domainKearneyLinesAll,1/resamplingFactor,'nearest');
 [nzX, nzY, nzZ] = ind2sub(size(data), find(data > 1));
 
-% get x, y, z indices of zero elements
 [zX, zY, zZ] = ind2sub(size(data), find(data <= 1));
 
-% build vectors of x, y, z coordinates and corresponding values
 nonzero_points = [nzX, nzY, nzZ];
 nonzero_values = data(data >1);
 
-% create scattered interpolant with inverse distance weighting
 F = scatteredInterpolant(nonzero_points, double(nonzero_values), 'natural');
 
-% now interpolate the zero values
 zero_points = [zX, zY, zZ];
 interpolated_values=zeros(size(zero_points,1),1);
 for i=1:size(zero_points,1)
@@ -120,7 +91,6 @@ for i=1:size(zero_points,1)
     interpolated_values(i) = F(zero_points(i,:));
 end
 
-% replace zero values in the data
 data(data <= 1) = interpolated_values;
 data=imresize3(data,resamplingFactor);
 data(~solid)=0;
@@ -131,17 +101,13 @@ writeTiffStackYDW('./domainKearneyInterp.tif',data);
 data=imresize3(domainSunLinesAll,1/resamplingFactor,'nearest');
 [nzX, nzY, nzZ] = ind2sub(size(data), find(data > 1));
 
-% get x, y, z indices of zero elements
 [zX, zY, zZ] = ind2sub(size(data), find(data <= 1));
 
-% build vectors of x, y, z coordinates and corresponding values
 nonzero_points = [nzX, nzY, nzZ];
 nonzero_values = data(data >1);
 
-% create scattered interpolant with inverse distance weighting
 F = scatteredInterpolant(nonzero_points, double(nonzero_values), 'natural');
 
-% now interpolate the zero values
 zero_points = [zX, zY, zZ];
 interpolated_values=zeros(size(zero_points,1),1);
 for i=1:size(zero_points,1)
@@ -151,7 +117,6 @@ for i=1:size(zero_points,1)
     interpolated_values(i) = F(zero_points(i,:));
 end
 
-% replace zero values in the data
 data(data <= 1) = interpolated_values;
 data=imresize3(data,resamplingFactor);
 data(~solid)=0;
@@ -164,17 +129,13 @@ writeTiffStackYDW('./domainSunInterp.tif',data);
 data=imresize3(domainDirectLinesAll,1/resamplingFactor,'nearest');
 [nzX, nzY, nzZ] = ind2sub(size(data), find(data > 1));
 
-% get x, y, z indices of zero elements
 [zX, zY, zZ] = ind2sub(size(data), find(data <= 1));
 
-% build vectors of x, y, z coordinates and corresponding values
 nonzero_points = [nzX, nzY, nzZ];
 nonzero_values = data(data >1);
 
-% create scattered interpolant with inverse distance weighting
 F = scatteredInterpolant(nonzero_points, double(nonzero_values), 'natural');
 
-% now interpolate the zero values
 zero_points = [zX, zY, zZ];
 interpolated_values=zeros(size(zero_points,1),1);
 for i=1:size(zero_points,1)
@@ -184,7 +145,6 @@ for i=1:size(zero_points,1)
     interpolated_values(i) = F(zero_points(i,:));
 end
 
-% replace zero values in the data
 data(data <= 1) = interpolated_values;
 data=imresize3(data,resamplingFactor);
 data(~solid)=0;
